3bdb93b678d4bde1c7a2ab530fedafbb67683d6e
Sarmad <sarmadrehan52@gmail.com>
2022-04-25 11:49:10 +0500

    Update

--- recipe.dart
+++ recipe.dart
@@ -5,20 +5,25 @@ class Recipe {
   final int duration;
   final List<String> ingredients;
   final List<String> steps;
-  final bool isGlutentFree;
+  final bool isGlutenFree;
   final bool isVegan;
   final bool isVegetarian;
   final bool isLactoseFree;
 
-  Recipe(
+  const Recipe(
       {required this.id,
       required this.title,
       required this.imageUrl,
       required this.duration,
       required this.ingredients,
       required this.steps,
-      required this.isGlutentFree,
+      required this.isGlutenFree,
       required this.isLactoseFree,
       required this.isVegan,
       required this.isVegetarian});
+
+  @override
+  String toString() {
+    return "\n\nId: $id Title:$title ImageURL:$imageUrl Duration:$duration Ingredients:$ingredients Steps: $steps IsGlutentFree: $isGlutenFree IsVegan: $isVegan IsVegetarian: $isVegetarian IsLactoseFree:$isLactoseFree";
+  }
 }
