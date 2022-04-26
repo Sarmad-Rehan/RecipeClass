095bcede9e5050cbc49add1e95100962c8d82b21
Sarmad <sarmadrehan52@gmail.com>
2022-04-26 09:38:07 +0500

    Where & Map Update

--- main.dart
+++ main.dart
@@ -1,11 +1,12 @@
 import 'dummy_recipes.dart';
 import 'recipe.dart';
+import 'selected_recipes.dart';
 
 void main(List<String> args) {
-  List<Recipe> list =
+  List<Recipe> listId =
       dummy_recipes.where((element) => element.id == 'm1').toList();
   print("List ID");
-  print(list);
+  print(listId);
   List<Recipe> listDuration =
       dummy_recipes.where((element) => element.duration > 20).toList();
   print("List Duration");
@@ -24,4 +25,18 @@ void main(List<String> args) {
       dummy_recipes.where((element) => element.title.startsWith("C")).toList();
   print("List Title");
   print(listTitle);
+
+  List<SelectedRecipe> selectedList = dummy_recipes
+      .map(
+          (e) => SelectedRecipe(id: e.id, title: e.title, duration: e.duration))
+      .where((element) => element.duration >= 20)
+      .toList();
+  print(selectedList);
+
+  List<SelectedRecipe> list = dummy_recipes
+      .where((element) => element.duration >= 20)
+      .map(
+          (e) => SelectedRecipe(id: e.id, title: e.title, duration: e.duration))
+      .toList();
+  print(list);
 }
